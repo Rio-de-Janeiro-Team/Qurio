e53587322f8ba376fe9b168e3974b44d7362a3a7
Noor Serry <nour.serry.94@gmail.com>
2025-09-28 14:21:14 +0300

    feature : add the base fragment class

---
+++ app/src/main/java/com/example/qurio/presentation/view/base/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.example.qurio.presentation.view.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = _binding
+            ?: throw IllegalStateException("Binding is null. Call this between onCreateView and onDestroyView.")
+
+    protected abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(layoutInflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
