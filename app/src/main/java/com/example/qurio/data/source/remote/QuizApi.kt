fee0a0cdc920aaa26cf2a6e7faeece4ef762f047
Mona <monaayman584@gmail.com>
2025-10-14 20:14:13 +0300

    refactor: add function to get Categories

--- app/src/main/java/com/example/qurio/data/source/remote/QuizApi.kt
+++ app/src/main/java/com/example/qurio/data/source/remote/QuizApi.kt
@@ -1,15 +1,18 @@
 package com.example.qurio.data.source.remote
 
+import com.example.qurio.data.response.CategoryResponseDtoDto
 import com.example.qurio.data.response.QuizResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface QuizApi{
     @GET("api.php")
-  suspend fun getQuestions(
+  suspend fun getQuiz(
         @Query("amount") amount: Int,
         @Query("category") category: Int,
         @Query("difficulty") difficulty: String?,
     ): QuizResponseDto
 
+  @GET("api_category.php")
+  suspend fun getCategories(): CategoryResponseDtoDto
  }
--- app/src/main/java/com/example/qurio/data/source/remote/QuizApiService.kt
+++ app/src/main/java/com/example/qurio/data/source/remote/QuizApiService.kt
@@ -1,5 +1,6 @@
 package com.example.qurio.data.source.remote
 
+import com.example.qurio.data.response.CategoryResponseDtoDto
 import com.example.qurio.data.response.QuizResponseDto
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,12 +11,16 @@ class QuizApiService: QuizApi {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     val quizApi: QuizApi = retrofit.create(QuizApi::class.java)
-   override suspend fun getQuestions(
+   override suspend fun getQuiz(
         amount: Int,
         category: Int,
         difficulty: String?,
     ): QuizResponseDto {
-        return quizApi.getQuestions(amount, category, difficulty)
+        return quizApi.getQuiz(amount, category, difficulty)
+    }
+
+    override suspend fun getCategories(): CategoryResponseDtoDto {
+        return quizApi.getCategories()
     }
 
 }
