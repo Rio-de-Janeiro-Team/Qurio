f1c03b7021b3fc4ba90649b786284cc9f7d5fcce
yousef_osama11 <oyousef709@gmail.com>
2025-10-17 02:20:33 +0300

    feat: add game adapter

---
+++ app/src/main/java/com/example/qurio/presentation/view/game/GameAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.qurio.presentation.view.game
+
+import com.example.qurio.BR
+import com.example.qurio.R
+import com.example.qurio.presentation.view.base.BaseAdapter
+
+class GameAdapter(var answers: List<Answer>) : BaseAdapter<Answer>(items = answers) {
+    override val layoutID: Int = R.layout.item_answer
+
+    override fun bind(holder: ItemViewHolder, position: Int) {
+        if (answers.isNotEmpty()){
+            val currentAnswer = answers[position]
+            holder.binding.setVariable(BR.item,currentAnswer)
+        }
+    }
+
+    override fun setItems(newItems: List<Answer>) {
+        super.setItems(newItems)
+        answers = newItems
+    }
+}
+
